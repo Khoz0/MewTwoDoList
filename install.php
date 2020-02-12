04aa2352d03bc7fcd164619aa4db8ed1128e2a25
Pagalol <louis.matuchet54@gmail.com>
2020-02-12 18:22:59 +0000

    install.php

--- install.php
+++ install.php
@@ -1,1 +1,22 @@
-<?php // Pour louis
+<?php
+
+try {
+$config = parse_ini_file("src/Config/config.ini");
+} catch(Exception $e) {
+	echo "Merci de creer un fichier de configuration dans 'src/Config/config.ini'";
+	die;
+}
+
+$query = file_get_contents("sql.sql");
+
+
+
+require 'src/Classes/DB.php';
+$bdd = App\Classes\DB::getInstance();
+
+$stmt = $bdd->prepare($query);
+
+if ($stmt->execute())
+     echo "Succes";
+else
+     echo "Echec";
