105e8825e221dc9dd931ff2fb3d48d1010d6f836
xandermann <contact@alexandre-hublau.com>
2020-02-20 02:15:12 +0100

    Idee

--- src/Controllers/ListeController.php
+++ src/Controllers/ListeController.php
@@ -12,6 +12,14 @@ class ListeController extends Controller {
     public function liste() {
     	$id = $_GET['id'];
     	$bdd = serialize(DB::getInstance()->loadListe($id));
+
+    	/*
+    	Faire : si pas de résultat, alors affiche erreur 404 ...
+    	if (is_null($bdd)) {
+    		// Exception ici ?
+    	}
+    	*/
+
         return $this->render('liste', compact('bdd', 'id'));
     }
 
