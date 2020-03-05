7d76eb127d98767ea948e33fccc7ec7940335e3d
Florian Poirel <poirel31u@fst-ats-311-l19.fst.site.univ-lorraine.fr>
2020-03-05 12:05:03 +0100

    debut test inscription

---
+++ tests/Controllers/InscriptionControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace Tests\Controllers;
+
+use App\Controllers\AccueilController;
+use PHPUnit\Framework\TestCase;
+use App\Modeles\DB;
+
+
+/**
+ * Tests InscriptionControllerTest
+ */
+class InscriptionControllerTest extends TestCase {
+
+	$fonction = new FonctionTest();
+    	
+		$res = false;
+
+		$bdd = DB::getInstance()->getPDO();
+}
