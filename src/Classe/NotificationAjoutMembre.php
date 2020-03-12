e1b73da23b980adcafbd78e29254bc8621ec8fe4
jordan <scherring.jordan@gmail.com>
2020-03-12 10:33:28 +0100

    classe Notification Ajout Membre

---
+++ src/Classe/NotificationAjoutMembre.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Classe;
+
+use App\Modeles\DB;
+
+abstract class NotificationAjoutMembre extends NotificationAvecChoix {
+
+  function __construct($dateCreation, $contenu, $source) {
+      parent::__construct($dateCreation, $contenu, $source);
+    }
+
+
+    public ajouterBDD() {
+      //AjouterBDD
+    }
+
+    public supprimerBDD() {
+      //SupprimerBDD
+    }
+
+    public ajoutMembre() {
+
+    }
+
+
+  ?>
