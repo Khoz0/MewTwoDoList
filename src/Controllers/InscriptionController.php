5b499575dd257070a2581a4bdd9886cf57f4de37
adele <adele.barbier1@etu.univ-lorraine.fr>
2020-03-23 08:30:25 +0100

    Modification enregistrement de la photo

--- src/Controllers/InscriptionController.php
+++ src/Controllers/InscriptionController.php
@@ -67,8 +67,8 @@ class InscriptionController extends Controller
                 mkdir("assests/uploads/", 0777, true);
             }
             str_replace('http', '', $photo['name']);
-            move_uploaded_file($photo['tmp_name'], 'assests/uploads/' . basename($photo['name']).$mail);
-            $photo = "assests/uploads/".$photo['name'].$mail;
+            move_uploaded_file($photo['tmp_name'], 'assests/uploads/' .$mail. basename($photo['name']));
+            $photo = "assests/uploads/".$mail.$photo['name'];
         } else {
             $photo = "assests/profil.png";
         }
